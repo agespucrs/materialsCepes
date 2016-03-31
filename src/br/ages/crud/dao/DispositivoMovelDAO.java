7f85e7912bd6904cb9ddca58fe3e21beb687b6ea
Vinicius Ferreira <vini.g.fer@gmail.com>
2016-03-31 18:20:20 -0300

    - Adicionado DAO e BO para dispositivos moveis

---
+++ src/br/ages/crud/dao/DispositivoMovelDAO.java
@@ -0,0 +1,24 @@
+package br.ages.crud.dao;
+
+import java.util.List;
+
+import br.ages.crud.model.DispositivoMovel;
+
+public class DispositivoMovelDAO {
+
+	public void cadastrarDispositivoMovel(DispositivoMovel dispositivoMovel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<DispositivoMovel> listarDispositivosMoveis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void alterarDispositivoMovel(DispositivoMovel dispositivoMovel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
