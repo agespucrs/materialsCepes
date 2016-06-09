0e02fe42a0e0fd0a1fe6f4e4e24a0d74b91a1ad2
unknown <15280426@W3070638.portoalegre.pucrsnet.br>
2016-06-09 18:28:32 -0300

    Correcao paliativa para o problema na listagem de autores

--- src/br/ages/crud/command/ListAutorCommand.java
+++ src/br/ages/crud/command/ListAutorCommand.java
@@ -14,23 +14,29 @@ public class ListAutorCommand implements Command {
 
 	private String proxima;
 	private AutorBO autorBO;
-	
+
 	@Override
 	public String execute(HttpServletRequest request) throws SQLException {
 		this.autorBO = new AutorBO();
 		proxima = "autor/listAutor.jsp";
-		String idLivroString = request.getParameter("id_livro");
-		
+
+		String listarTodos = request.getParameter("listarTodos");
+
 		try {
 			List<Autor> listaAutores = autorBO.listarTodosAutores();
 			request.setAttribute("listaAutores", listaAutores);
-			
-			int idLivro = Integer.parseInt(idLivroString);
-			Livro livro = new Livro();
-			livro.setIdLivro(idLivro);
-			
-			List<Autor> listaAutoresLivro = autorBO.listarAutorLivro(livro);
-			request.setAttribute("listaAutoresLivro", listaAutoresLivro);
+
+			if (!"S".equals(listarTodos)) {
+
+				String idLivroString = request.getParameter("id_livro");
+				
+				int idLivro = Integer.parseInt(idLivroString);
+				Livro livro = new Livro();
+				livro.setIdLivro(idLivro);
+
+				List<Autor> listaAutoresLivro = autorBO.listarAutorLivro(livro);
+				request.setAttribute("listaAutoresLivro", listaAutoresLivro);
+			}
 		} catch (NegocioException e) {
 			e.printStackTrace();
 			request.setAttribute("msgErro", e.getMessage());
